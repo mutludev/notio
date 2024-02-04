172acf0566c72a3a15f79cab185c2fe3056f8465
Barış Mutlu <baris@mutlu.dev>
2024-02-04 22:45:00 +0300

    Add home layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,32 @@
 <script setup>
 import NoteSheet from "@/components/note-sheet.vue";
+import ControlButtons from "@/components/control-buttons.vue";
+import Metronome from "@/components/metronome.vue";
+import { onMounted } from "vue";
 import { useNoteSheetStore } from "@/stores/note-sheet";
 const noteSheetStore = useNoteSheetStore();
 
 </script>
 
 <template>
-  <note-sheet />
+  <div class="container">
+        <metronome />
+        <note-sheet />
+      <control-buttons /> 
+  </div>
 </template>
 
 <style scoped>
+.container {
+  height: 100vh;
+  width: 100%;
+  background-color: #f9f7f4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+}
+
 </style>
 
