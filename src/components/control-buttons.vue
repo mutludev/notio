582a87f07199bd211b14c33d566e61b2375aeaa8
Barış Mutlu <baris@mutlu.dev>
2024-02-04 22:25:48 +0300

    Add buttons component

---
+++ src/components/control-buttons.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useNoteSheetStore } from "@/stores/note-sheet";
+const noteSheetStore = useNoteSheetStore();
+
+</script>
+
+<template>
+    <div class="buttons">
+        <button @click="noteSheetStore.startPlaying">New Notes</button>
+        <button @click="noteSheetStore.resetPlaying">Restart Playing</button>
+    </div>
+</template>
+
+<style scoped>
+.buttons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
+.buttons button {
+  padding: 10px 20px;
+  font-size: 1.2rem;
+  border: none;
+  border-radius: 5px;
+  background-color: hsl(42, 31%, 60%);
+  color: black;
+  cursor: pointer;
+}
+</style>
