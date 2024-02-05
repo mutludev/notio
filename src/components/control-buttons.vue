58e3d516d93f04be14297aff81e16e83650f9ea1
Barış Mutlu <baris@mutlu.dev>
2024-02-05 20:37:35 +0300

    Refactor buttons

--- src/components/control-buttons.vue
+++ src/components/control-buttons.vue
@@ -2,24 +2,47 @@
 import { useNoteSheetStore } from "@/stores/note-sheet";
 const noteSheetStore = useNoteSheetStore();
 
+function togglePlaying() {
+  if(noteSheetStore.playing) {
+    noteSheetStore.pausePlaying()
+  } else {
+    noteSheetStore.startPlaying()
+  }
+}
+
 </script>
 
 <template>
     <div class="buttons">
-        <button @click="noteSheetStore.randomizeNotes">New Notes</button>
-        <button @click="noteSheetStore.resetPlaying">Restart Playing</button>
+        <div>
+          <button @click="togglePlaying">{{ noteSheetStore.playing ? 'Stop' : 'Play' }}</button>
+          <button @click="noteSheetStore.resetPlaying">Restart</button>
+        </div>
+        <div>
+          <button @click="noteSheetStore.randomizeNotes">New Notes</button>
+        </div>
     </div>
 </template>
 
 <style scoped>
 .buttons {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 10px;
+}
+
+.buttons div {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
   gap: 20px;
 }
 
 .buttons button {
+  min-width: 150px;
+  width: 100%;
   padding: 10px 20px;
   font-size: 1.2rem;
   border: none;
