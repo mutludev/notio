da2581c2fbb3d95a51a1e0bccec60fe2b75337e1
Barış Mutlu <baris@mutlu.dev>
2024-02-05 22:59:51 +0300

    Make notesheet scroll

--- src/components/note-sheet.vue
+++ src/components/note-sheet.vue
@@ -1,20 +1,34 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, computed } from 'vue'
 import { useNoteSheetStore } from '@/stores/note-sheet'
 
 const noteSheetStore = useNoteSheetStore()
 
+
+
 onMounted(() => {
   noteSheetStore.initialize('notes')
 })
+
+let offsetPixel = computed(() => {
+  return noteSheetStore.lineOffsetPixels
+})
+
 </script>
 
 <template>
   <div id="notes"></div>
 </template>
 
-<style scoped>
+<style>
 #notes {
   height: 200px !important;
+  position: relative;
 }
+
+#notes svg {
+  transform: translate(0, v-bind(offsetPixel)); 
+  transform-origin: 0px 0px 0px;
+  transition: all 500ms;
+  }
 </style>
